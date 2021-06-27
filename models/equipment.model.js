8cd3c565ce0b9763229d945c89e2e17dcb86b52a
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-06-27 17:48:37 +0300

    fix

--- models/equipment.model.js
+++ models/equipment.model.js
@@ -3,10 +3,10 @@
 const mongoose = require("mongoose");
 
 const equipmentSchema = new mongoose.Schema({
-  title: { type: String },
-  discription: { type: String },
-  id: { type: Number },
-  quantity: {type: Number},
+    title: { type: String },
+    description: { type: String },
+    id: { type: Number },
+    quantity: { type: Number },
 });
 
 module.exports = equipmentSchema;
--- server.js
+++ server.js
@@ -1,32 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const axios = require('axios');
-const cors = require('cors');
+const axios = require("axios");
+const cors = require("cors");
 app.use(cors());
-require('dotenv').config();
+require("dotenv").config();
 app.use(express.json());
 const PORT = process.env.PORT;
 
-const mongoose =require('mongoose');
+const mongoose = require("mongoose");
 
-const getEqupiment= require('./controller/equipment.controller')
-const {seedUserData} = require('./models/user.model')
-mongoose.connect('mongodb://localhost:27017/homegym',
-{ useNewUrlParser: true, useUnifiedTopology: true }
-);
+const getEquipment = require("./controller/equipment.controller");
+const { seedUserData } = require("./models/user.model");
+mongoose.connect("mongodb://localhost:27017/homegym", { useNewUrlParser: true, useUnifiedTopology: true });
 
 ////// end Server
 
+// seedUserData()
 
-seedUserData()
+app.get("/", function(req, res) {
+    res.send("Hello Hero's");
+});
 
-
-app.get('/', 
- function (req, res) { 
-  res.send('Hello Hero\'s') 
-})
-
-app.get('/profile',getEqupiment)
-app.listen(PORT,()=>{
+app.get("/profile", getEquipment);
+app.listen(PORT, () => {
     console.log(`running on port ${PORT}`);
-}) 
+});
