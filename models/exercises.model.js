b4836345c74fcbf17d33d2855531bcb0b6e4c20e
Yaser Al-Ananbeh <yaser.ananbeh74@gmail.com>
2021-06-29 01:09:53 +0300

    send exercise image

--- models/exercises.model.js
+++ models/exercises.model.js
@@ -2,6 +2,7 @@ class ExerciseModel {
     constructor(obj) {
         this.name = obj.name;
         this.desc = obj.description.replace(/<[^>]*>/g, "");
+        this.image_url=obj.image_url;
     }
 }
 module.exports = ExerciseModel;
